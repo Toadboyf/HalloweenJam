4d7087af2241700bbee6f941587a6ba3bf4ff2c9
Toadboyf <Zmartens93@gmail.com>
2020-10-04 20:37:58 -0400

    stuff

---
+++ HalloweenJam/Assets/_Scripts/PlayerInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    bool interacting;
+    public float interactDist;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Interact();
+        }
+    }
+
+    void Interact()
+    {
+        if(!interacting)
+        {
+            interacting = true;
+            RaycastHit hit;
+            Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactDist);
+            
+        }
+    }
+}
